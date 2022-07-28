db1159683ce2a823d075b21bed69bb761cec82e0
niMgnoeSeeL <nim.gnoes.eel@gmail.com>
2022-07-28 16:20:35 +0900

    Add wss4j archiva deltaspike systemds; there is some error in systemds

--- script/run2.sh
+++ script/run2.sh
@@ -1,3 +1,3 @@
-for proj in net collections beanutils codec compress; do
+for proj in lang net collections beanutils codec compress; do
     python gitparse.py $proj >> log/log2.txt 2>> log/err2.txt
 done
